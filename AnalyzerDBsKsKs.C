b437f4714cd9e124e6114bd7a9fff664f0b6b333
ferdani <danisheldoncooper@gmail.com>
2018-03-02 15:48:03 +0100

    Añadidos histogramas nuevos. Comienzo analisis seleccion NewCuts
    Se implementan nuevos histogramas para comenzar el proceso de refinar
    la seleccion de cortes inicial que ahora conocemos como OldCuts.
    El analisis independiente de cada variable permitirá corregir esta
    primera selección en lo que se está implementando como NewCuts.

--- AnalyzerDBsKsKs.C
+++ AnalyzerDBsKsKs.C
@@ -6,11 +6,17 @@
 #include <iostream>
 
 //histogramas
-TH1F* H_InvariantMass = new TH1F("H_InvMass", "Invariant Mass kpikpi", 100, 4700, 6000);
+TH1F* H_InvariantMassOldCuts = new TH1F("H_InvMass", "Invariant Mass kpikpi Old Cuts", 100, 4700, 6000);
+TH1F* H_InvariantMassNewCuts = new TH1F("H_InvMass", "Invariant Mass kpikpi New Cuts", 100, 4700, 6000);
+
 TH1F* H_pip_PT = new TH1F("H_pip_PT", "PT of pion plus", 100, 0, 8000);
 TH1F* H_pim_PT = new TH1F("H_pim_PT", "PT of pion minus", 100, 0, 8000);
 TH1F* H_Kp_PT = new TH1F("H_Kp_PT", "PT of kaon plus", 100, 0, 7000);
 TH1F* H_Km_PT = new TH1F("H_Km_PT", "PT of kaon minus", 100, 0,7000);
+TH1F* H_Kst_PT = new TH1F("H_Kst_PT", "PT of k star", 200, 0,16000);
+TH1F* H_Kstb_PT = new TH1F("H_Kstb_PT", "PT of k' star", 200, 0,16000);
+TH1F* H_Kst_M = new TH1F("H_Kst_M", "InvMass of k star", 200, 0, 2400);
+TH1F* H_Kstb_M = new TH1F("H_Kstb_M", "InvMass of k' star", 200, 0, 2400);
 
 void AnalyzerDBsKsKs::Loop(){
 //   In a ROOT session, you can do:
@@ -32,7 +38,8 @@ void AnalyzerDBsKsKs::Loop(){
     // ---------------------------------------------------------------------------
     
 //inicializo las variables:
-   Float_t Mkpikpi = 0.0;	
+   Float_t MkpikpiOld = 0.0;
+   Float_t MkpikpiNew = 0.0;	
 
 //constuyendo el loop
    if (fChain == 0) return;
@@ -45,13 +52,19 @@ void AnalyzerDBsKsKs::Loop(){
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
 	  printProgBar(jentry*100./nentries);
-      //selección:
+      //---------------------------selection process: ---------------------------------------------------------
+      
       H_pim_PT->Fill(pim_PT);
       H_pip_PT->Fill(pip_PT);
       H_Kp_PT->Fill(Kp_PT);
       H_Km_PT->Fill(Km_PT);
+      H_Kst_PT->Fill(Kst_PT);
+      H_Kstb_PT->Fill(Kstb_PT);
+      H_Kst_M->Fill(Kst_M);
+      H_Kstb_M->Fill(Kstb_M);
       
-      if(pip_PT>500 && Km_PT>500 && pim_PT>500 && Kp_PT>500){
+      //Old selection:
+      if(pip_PT>500. && Km_PT>500. && pim_PT>500. && Kp_PT>500.){
 		  
 		  if(pip_IPCHI2_OWNPV>36. && Km_IPCHI2_OWNPV>36. && pim_IPCHI2_OWNPV>36. && Kp_IPCHI2_OWNPV>36.){
 			  
@@ -80,21 +93,67 @@ void AnalyzerDBsKsKs::Loop(){
 										
 													//calculo masa invariante de los cuatro cuerpos
 													
-													Mkpikpi=(kplus+piminus+kminus+piplus).M();
-													H_InvariantMass->Fill(Mkpikpi);
+													MkpikpiOld=(kplus+piminus+kminus+piplus).M();
+													H_InvariantMassOldCuts->Fill(MkpikpiOld);
 													
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+											  }
+									      }
+								      }
+								  }
+							  }
+						  }
+				      }
+				  }
+			  }
+	      }
+      }
+		
+	  //New selection:	
+	  if(pip_PT>490. && Km_PT>490. && pim_PT>490. && Kp_PT>490.){
+		  
+		  if(pip_IPCHI2_OWNPV>36. && Km_IPCHI2_OWNPV>36. && pim_IPCHI2_OWNPV>36. && Kp_IPCHI2_OWNPV>36.){
+			  
+			  if(Km_PIDK>3.1 && Kp_PIDK>3.1){
+				  
+				  if(pip_PIDK<0. && pim_PIDK<0.){
+					  
+					  if(pip_TRACK_CHI2NDOF<5 && Km_TRACK_CHI2NDOF<5 && pim_TRACK_CHI2NDOF<5 && Kp_TRACK_CHI2NDOF<5){
+						  
+						  if((Km_PIDp-Km_PIDK)<0. && (Kp_PIDp-Kp_PIDK)<0. && pim_PIDp<0. && pip_PIDp<0.){
+							  
+							  if(Kstb_M<1600. && Kst_M<1600.){
+								  
+								  if(Kstb_PT>900. && Kst_PT>900.){
+									  
+									  if(Kstb_ENDVERTEX_CHI2<9. && Kst_ENDVERTEX_CHI2<9.){
+										  
+										  if(Kstb_DIRA_OWNPV>0. && Kst_DIRA_OWNPV>0.){
+											  
+											  if(pip_isMuon==0 && pim_isMuon==0 && Kp_isMuon==0 && Km_isMuon == 0){
+												  
+													kplus.SetPxPyPzE(Kp_PX,Kp_PY,Kp_PZ,Kp_PE);
+													kminus.SetPxPyPzE(Km_PX,Km_PY,Km_PZ,Km_PE);
+													piplus.SetPxPyPzE(pip_PX,pip_PY,pip_PZ,pip_PE);
+													piminus.SetPxPyPzE(pim_PX,pim_PY,pim_PZ,pim_PE);
+										
+													//calculo masa invariante de los cuatro cuerpos
+													
+													MkpikpiNew=(kplus+piminus+kminus+piplus).M();
+													H_InvariantMassNewCuts->Fill(MkpikpiNew);
+													
+											  }
+									      }
+								      }
+								  }
+							  }
+						  }
+				      }
+				  }
+			  }
+	      }
+      }
+      			
+   }
 cout << endl;	
 std::cout<< "100" << "%   ¡Complete!  " << std::flush << "\r" << endl;
 
@@ -113,11 +172,18 @@ void AnalyzerDBsKsKs::WriteHistos(){
   gSystem->mkdir(outputDir, true);
   out = new TFile(outputDir + "/Histos_" + sampleNameRoot + ".root", "RECREATE");
   
-  H_InvariantMass->Write(); 
+  H_InvariantMassOldCuts->Write();
+  H_InvariantMassNewCuts->Write();
+  
   H_pim_PT->Write();
   H_pip_PT->Write();
-  H_Km_PT->Write();
   H_Kp_PT->Write();
+  H_Km_PT->Write();
+  H_Kst_PT->Write();
+  H_Kstb_PT->Write();
+  H_Kst_M->Write();
+  H_Kstb_M->Write();
+  
   out->Close();
   delete out;
 }
