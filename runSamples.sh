a18c10f20f43de73b390b9bf00af121da7621a18
ferdani <danisheldoncooper@gmail.com>
2018-06-28 17:27:41 +0200

    Correccion del path hasta el Decay Tree del MC

--- runSamples.sh
+++ runSamples.sh
@@ -1,5 +1,5 @@
 root -l -b -q 'RunAnalysisBsKsKs.C("DecayTree", "DecayTree")'
 #root -l -b -q 'RunAnalysisBsKsKs.C("DTT_2016_Reco16Strip28_Down_BHADRON", "Bs2KpiKpi/DecayTree")'
 #root -l -b -q 'RunAnalysisBsKsKs.C("DTT_2016_Reco16Strip28_Up_BHADRON", "Bs2KpiKpi/DecayTree")'
-#root -l -b -q 'RunAnalysisBsKsKs.C("DTT_2016_Reco16Strip28_Down_BHADRON.root", "Bs2KpiKpi/DecayTree")'
-#root -l -b -q 'RunAnalysisBsKsKs.C("DTT_2016_Reco16Strip28_Up_BHADRON.root", "Bs2KpiKpi/DecayTree")'
+#root -l -b -q 'RunAnalysisBsKsKs.C("Bs2Kst0Kst0_wide_MC2012_magDown_Bs2Kst0Kst0_13104001_Job1555", "Bs2Kst0Kst0/DecayTree")'
+#root -l -b -q 'RunAnalysisBsKsKs.C("Bs2Kst0Kst0_wide_MC2012_magUp_Bs2Kst0Kst0_13104001_Job1555.root", "Bs2Kst0Kst0/DecayTree")'
