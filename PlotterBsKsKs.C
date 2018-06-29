45a939a19fd1c56d6064a6ce47f3599afb336ba5
ferdani <danisheldoncooper@gmail.com>
2018-06-29 10:42:14 +0200

    Modificacion del ploteador para data y MC
    Primera modificacion del ploteador para poder diferenciar entre Data y
    MC, del modo que puedan ser independientes y podamos modificarlos. Se
    plantea asi porque mas adelante tendremos que trabajar con mas datos
    de MC.

--- PlotterBsKsKs.C
+++ PlotterBsKsKs.C
@@ -11,37 +11,54 @@ const Float_t Lumi = 50.0;
 const TString outputDir = "./Plots";
 TString xt; //titulo variable para el eje x.
 
-TH1F* loadHistogram(TString sampleNameRoot, TString var){
+TH1F* loadHistogram(TString sampleNameRoot, TString var, TString Type){
   TFile* f;
   f = TFile::Open(pathToFiles + "Histos_" + sampleNameRoot + ".root");
   TH1F* h;
-  f->GetObject("H_"+var, h);
+  if(Type == "Data"){f->GetObject("H_"+var, h)};
+  if(Type == "MC"){f->GetObject("H_"+var+"_MC", h)};
   h->SetDirectory(0);
   delete f;
-  return h;
+  return h;	  
 }
 
 void PlotterBsKsKs(TString var){
-  TH1F* down  = loadHistogram("DTT_2016_Reco16Strip28_Down_BHADRON",       var);
-  TH1F* up = loadHistogram("DTT_2016_Reco16Strip28_Up_BHADRON",      var);
-
-  down->SetFillColor(kRed+1  );
-  up   ->SetFillColor(kBlue);
-
-  THStack* hStack = new THStack("Stack_"+var, "");
-  hStack->Add(down);
-  hStack->Add(up);
+  TH1F* down  = loadHistogram("DTT_2016_Reco16Strip28_Down_BHADRON", var, "Data");
+  TH1F* up = loadHistogram("DTT_2016_Reco16Strip28_Up_BHADRON", var, "Data");
+  TH1F* downMC  = loadHistogram("Bs2Kst0Kst0_wide_MC2012_magDown_Bs2Kst0Kst0_13104001_Job1555", var, "MC");
+  TH1F* upMC = loadHistogram("Bs2Kst0Kst0_wide_MC2012_magUp_Bs2Kst0Kst0_13104001_Job1555", var, "MC");
 
   TLegend* leg = new TLegend(0.8,0.8,0.98,0.98);
-  leg->AddEntry(down, Form("down : %5.2f", down->Integral() + down->GetBinContent(down->GetNbinsX()+2)), "f");
-  leg->AddEntry(up, Form("up : %5.2f", up->Integral() + up->GetBinContent(up->GetNbinsX()+2)), "f");
+  	
+  if(Type == "Data"){	
+	down->SetFillColor(kRed+1  );
+	up->SetFillColor(kBlue);
+	THStack* hStack = new THStack("Stack_"+var, "");
+	hStack->Add(down);
+    hStack->Add(up);
+    
+    leg->AddEntry(down, Form("down : %5.2f", down->Integral() + down->GetBinContent(down->GetNbinsX()+2)), "f");
+    leg->AddEntry(up, Form("up : %5.2f", up->Integral() + up->GetBinContent(up->GetNbinsX()+2)), "f");
+  }
+  
+  if(Type == "MC"){
+	downMC->SetFillColor(kRed+1  );
+	upMC->SetFillColor(kBlue); 
+    THStack* hStackMC = new YHStack("Stack_"+var+"_MC", "");
+	hStackMC->Add(downMC);
+	hStackMC->Add(upMC); 
+	
+	leg->AddEntry(downMC, Form("downMC : %5.2f", downMC->Integral() + down->GetBinContent(down->GetNbinsX()+2)), "f");
+    leg->AddEntry(upMC, Form("upMC : %5.2f", upMC->Integral() + up->GetBinContent(up->GetNbinsX()+2)), "f");	  
+  }	  
 
   TCanvas *c = new TCanvas("c","c",10,10,800,600);
 
   hStack->Draw("hist");
+  hStackMC->Draw("hist");
   leg->Draw();
 
-  if((var == "InvMassNewCuts") || (var == "InvMassOldCuts")) xt = ("M_{k #pi k #pi} [GeV]");
+  if((var == "InvMass") || (var == "Bs0_M")) xt = ("M_{k #pi k #pi} [GeV]");
   if(var == "pip_PT") xt = ("PT_{pip} [GeV]^{2}");
   if(var == "pim_PT") xt = ("PT_{pim} [GeV]^{2}");
   if(var == "Kp_PT") xt = ("PT_{Kp} [GeV]^{2}");
--- RunAnalysisBsKsKs.C
+++ RunAnalysisBsKsKs.C
@@ -1,13 +1,13 @@
 //R__LOAD_LIBRARY(AnalyzerDBsKsKs.C+)
 #include "AnalyzerDBsKsKs.C"
 
-const Int_t   nSamples = 1;
+const Int_t   nSamples = 2;
 const Int_t   nsamples = 4;
 const Int_t   nOptions = 2;
 
-const TString SamplesRoot[nsamples] = {"DTT_2016_Reco16Strip28_Down_BHADRON", "DTT_2016_Reco16Strip28_Down_BHADRON", 
-										"DTT_2016_Reco16Strip28_Down_BHADRON.root", "DTT_2016_Reco16Strip28_Up_BHADRON.root"};
-const TString Samples[nSamples] = {"Bs2KpiKpi/DecayTree"};
+const TString SamplesRoot[nsamples] = {"DTT_2016_Reco16Strip28_Down_BHADRON", "DTT_2016_Reco16Strip28_Down_BHADRON",
+										"Bs2Kst0Kst0_wide_MC2012_magDown_Bs2Kst0Kst0_13104001_Job1555", "Bs2Kst0Kst0_wide_MC2012_magUp_Bs2Kst0Kst0_13104001_Job1555"};
+const TString Samples[nSamples] = {"Bs2KpiKpi/DecayTree", "Bs2Kst0Kst0/DecayTree"};
 const TString Type[nOptions] = {"Data", "MC"};
 
 void RunAnalysisBsKsKs(TString sampleNameRoot, TString sampleName, TString Type);
