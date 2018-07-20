7f29e45b2dfe4a74a5c091362d413a25895be286
ferdani <danisheldoncooper@gmail.com>
2018-07-20 20:19:15 +0200

    correcion espacios en blanco

--- PythonTreesCuts.py
+++ PythonTreesCuts.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3                                                                                                             
-# -*- coding: utf-8 -*-                                                                                                            
-'''                                                                                                                                
-Created on Fri Jul 20 16:00:47 2018                                                                                                
-                                                                                                                                   
-@author: macbookpro                                                                                                                
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Created on Fri Jul 20 16:00:47 2018
+
+@author: macbookpro
 '''
 
 import root_numpy as rn
@@ -13,11 +13,7 @@ PathToTrees = '/home3/daniel.fernandez/Arboles/'
 SampleName = 'DTT_2016_Reco16Strip28_Down_BHADRON.root'
 SampleNameTree ='Bs2KpiKpi/DecayTree'
 
-TreeDataBranches = ['pip_PT', 'Km_PT', 'pim_PT', 'Kp_PT', 'pip_IPCHI2_OWNPV', 'Km_IPCHI2_OWNPV', 'pim_IPCHI2_OWNPV', 'Kp_IPCHI2_OWNPV', 'Km_PIDK', '\
-Kp_PIDK', 'pip_PIDK', 'pim_PIDK', 'pip_TRACK_CHI2NDOF', 'Km_TRACK_CHI2NDOF', 'pim_TRACK_CHI2NDOF', 'Kp_TRACK_CHI2NDOF', 'Km_PIDp', 'Km_PIDK', 'Kp_PI\
-Dp', 'Kp_PIDK', 'pim_PIDp', 'pip_PIDp', 'Kstb_M', 'Kst_M', 'Kstb_PT', 'Kst_PT', 'Kstb_ENDVERTEX_CHI2', 'Kst_ENDVERTEX_CHI2', 'Kstb_DIRA_OWNPV', 'Kst\
-_DIRA_OWNPV', 'pip_isMuon', 'pim_isMuon', 'Kp_isMuon', 'Km_isMuon', 'Kp_PX', 'Kp_PY', 'Kp_PZ', 'Kp_PE', 'Km_PX', 'Km_PY', 'Km_PZ', 'Km_PE', 'pim_PX'\
-, 'pim_PY', 'pim_PZ', 'pim_PE', 'pip_PX', 'pip_PY', 'pip_PZ', 'pip_PE']
+TreeDataBranches = ['pip_PT', 'Km_PT', 'pim_PT', 'Kp_PT', 'pip_IPCHI2_OWNPV', 'Km_IPCHI2_OWNPV', 'pim_IPCHI2_OWNPV', 'Kp_IPCHI2_OWNPV', 'Km_PIDK', 'Kp_PIDK', 'pip_PIDK', 'pim_PIDK', 'pip_TRACK_CHI2NDOF', 'Km_TRACK_CHI2NDOF', 'pim_TRACK_CHI2NDOF', 'Kp_TRACK_CHI2NDOF', 'Km_PIDp', 'Km_PIDK', 'Kp_PIDp', 'Kp_PIDK', 'pim_PIDp', 'pip_PIDp', 'Kstb_M', 'Kst_M', 'Kstb_PT', 'Kst_PT', 'Kstb_ENDVERTEX_CHI2', 'Kst_ENDVERTEX_CHI2', 'Kstb_DIRA_OWNPV', 'Kst_DIRA_OWNPV', 'pip_isMuon', 'pim_isMuon', 'Kp_isMuon', 'Km_isMuon', 'Kp_PX', 'Kp_PY', 'Kp_PZ', 'Kp_PE', 'Km_PX', 'Km_PY', 'Km_PZ', 'Km_PE', 'pim_PX', 'pim_PY', 'pim_PZ', 'pim_PE', 'pip_PX', 'pip_PY', 'pip_PZ', 'pip_PE']
 
 PTCuts = 'pip_PT>500. & Km_PT>500. & pim_PT>500. & Kp_PT>500.'
 Track_chi2 = 'pip_IPCHI2_OWNPV>36. & Km_IPCHI2_OWNPV>36. & pim_IPCHI2_OWNPV>36. & Kp_IPCHI2_OWNPV>36.'
